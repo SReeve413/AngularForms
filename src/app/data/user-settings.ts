4c0476825741f2e312e7a5a04caca266d5deb3c5
Shane Reeve <shanereeve@gmail.com>
2022-07-08 22:03:13 -0600

    Data Binding in Angular Forms

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface UserSetting {
+  name: string,
+  emailOffers: boolean,
+  interfaceStyle: string,
+  SubscriptionType: string,
+  notes: string,
+}
--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSetting } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-setting-form',
   templateUrl: './user-setting-form.component.html',
-  styleUrls: ['./user-setting-form.component.scss']
+  styleUrls: ['./user-setting-form.component.scss'],
 })
 export class UserSettingFormComponent implements OnInit {
+  originalUserSettings: UserSetting = {
+    name: 'Shane',
+    emailOffers: false,
+    interfaceStyle: 'light',
+    SubscriptionType: 'Annual',
+    notes: 'Here is a note...',
+  };
 
-  constructor() { }
+  userSettings: UserSetting = {... this.originalUserSettings};
+  constructor() {}
 
   ngOnInit(): void {
-  }
 
+  }
 }
